672d150066d8e7a43d7b57d8056bc004b4610bb6
yanwei <yanwei1983@gmail.com>
2019-05-18 01:48:10 +0800

    Update test_multireturn.cpp

--- tests/test_multireturn.cpp
+++ tests/test_multireturn.cpp
@@ -1,6 +1,15 @@
 #include "lua_tinker.h"
 #include"test.h"
 extern std::map<std::string, std::function<bool()> > g_test_func_set;
+std::tuple<int, int> push_tuple()
+{
+	return std::make_tuple(7,10);
+}
+
+bool test_tuple(std::tuple<int, int> tuple)
+{
+	return std::get<0>(tuple) == 8 && std::get<1>(tuple) == 9;
+}
 
 void test_multireturn(lua_State* L)
 {
@@ -51,6 +60,34 @@ void test_multireturn(lua_State* L)
 		lua_tinker::set_error_callback(&lua_tinker::on_error);
 
 		return true;
+	};
+	
+	
+	
+	g_test_func_set["test_push_tuple"] = [L]()->bool
+	{
+		std::string luabuf =
+			R"(function test_push_tuple()
+					local tuple_table = push_tuple();
+					return tuple_table[1] == 7 and tuple_table[2] == 10;
+				end
+			)";
+		lua_tinker::dostring(L, luabuf.c_str());
+
+		return lua_tinker::call<bool>(L, "test_push_tuple");
+	};
+	
+	g_test_func_set["test_get_tuple"] = [L]()->bool
+	{
+		std::string luabuf =
+			R"(function test_get_tuple()
+					local tuple_table = {8, 9};
+					return test_tuple(tuple_table);
+				end
+			)";
+		lua_tinker::dostring(L, luabuf.c_str());
+
+		return lua_tinker::call<bool>(L, "test_get_tuple");
 	};
 
 }
